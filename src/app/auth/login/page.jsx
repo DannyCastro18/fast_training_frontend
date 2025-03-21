7dfd4ecc3ded4743cc13cf70919f6f49b2ef170b
dev-espinaledu <espinalsolarte7@gmail.com>
2025-03-21 13:34:21 -0500

    cambio en login

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -44,7 +44,7 @@ export default function LoginPage() {
                             <input
                                 type="email"
                                 className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                                placeholder="usuario@correo.com"
+                                placeholder="user@example.com"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
