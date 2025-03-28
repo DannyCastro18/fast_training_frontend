695d33de6071a41e9b6e70b6cad2e0daec84f658
Andres <ffelipeandres24@gmail.com>
2025-03-27 22:53:50 -0500

    hice las estadisticas

--- src/app/entrenador/inicio/page.jsx
+++ src/app/entrenador/inicio/page.jsx
@@ -1,11 +1,13 @@
 "use-client";
 
 import Calendario from "../../../components/Calendario";
+import Estadisticas from "../../../components/Estadisticas"
 
 export default function inicioEntrenador() {
   return (
     <div className="flex flex-col items-center justify-center">
       <Calendario />
+      <Estadisticas />
     </div>
   );
 }
