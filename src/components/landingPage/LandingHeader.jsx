7d7ed8233a194ebdfb52ca4cd936acec118f15b8
dev-espinaledu <espinalsolarte7@gmail.com>
2025-03-29 14:47:04 -0500

    Mejor organización en las carpetas

--- src/components/landingPage/LandingHeader.jsx
+++ src/components/landingPage/LandingHeader.jsx
@@ -4,29 +4,29 @@ import { FaMoon, FaBell } from "react-icons/fa";
 import Link from "next/link";
 
 const LandingHeader = () => {
-  return (
-    <nav className="bg-[#205088] p-4 flex items-center justify-between">
-      <div className="flex items-center">
-        <img src="/icons/logo.jpg" alt="Fast Training Logo" className="h-10 mr-2" />
-        <span className="text-white text-xl font-bold">FAST TRAINING</span>
-      </div>
-      <div className="flex space-x-6 text-white">
-        <a href="#inicio" className="hover:underline">
-          Inicio
-        </a>
-        <a href="#acerca" className="hover:underline">
-          Acerca de
-        </a>
-      </div>
-      <div className="flex space-x-4">
-        <Link href="/auth/login">
-          <button className="bg-white text-blue-700 px-4 py-2 rounded-md">
-            Ingresar
-          </button>
-        </Link>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="bg-[#205088] p-4 flex items-center justify-between">
+        <div className="flex items-center">
+            <img src="/icons/logo.jpg" alt="Fast Training Logo" className="h-10 mr-2" />
+            <span className="text-white text-xl font-bold">FAST TRAINING</span>
+        </div>
+        <div className="flex space-x-6 text-white">
+            <a href="#inicio" className="hover:underline">
+            Inicio
+            </a>
+            <a href="#acerca" className="hover:underline">
+            Acerca de
+            </a>
+        </div>
+        <div className="flex space-x-4">
+            <Link href="/auth/login">
+            <button className="bg-white text-blue-700 px-4 py-2 rounded-md">
+                Ingresar
+            </button>
+            </Link>
+        </div>
+        </nav>
+    );
 };
 
 export default LandingHeader;
