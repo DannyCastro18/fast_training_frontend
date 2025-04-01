1e0aacf72391e7a6ac809090aeee2150ac149136
ffelipeandres24 <riverazunigaandresfelipe@gmail.com>
2025-04-01 17:56:32 -0500

    cree los destacados

--- src/app/entrenador/inicio/page.jsx
+++ src/app/entrenador/inicio/page.jsx
@@ -2,12 +2,14 @@
 
 import Calendario from "../../../components/Calendario";
 import Estadisticas from "@/components/Estadisticas";
+import FeaturedPlayers from "@/components/Destacados";
 
 export default function inicioEntrenador() {
   return (
     <div className="flex flex-col items-center justify-center">
       <Calendario />
       <Estadisticas />
+      <FeaturedPlayers />
     </div>
   );
 }
